dfe40963afe839c5bd025ce96e43d2ce0ce621ef
OliRamos <oli.ramos@ymail.com>
2023-06-27 13:38:26 -0300

    Implementando as restricoes de movimento

--- ConsoleChess/Xadrez/PartidaDeXadrex.cs
+++ ConsoleChess/Xadrez/PartidaDeXadrex.cs
@@ -6,14 +6,16 @@ namespace Xadrez
     class PartidaDeXadrex
     {
         public Tabuleiro tab { get; private set; }
-        private int turno;
-        private Cor jogadorAtual;
+        public int turno { get; private set; }
+        public Cor jogadorAtual {get; private set;}
         public bool terminada { get; private set; }
+
         public PartidaDeXadrex()
         {
             tab = new Tabuleiro(8, 8);
             turno = 1;
             jogadorAtual = Cor.Branca;
+            terminada = false;
             colocarPecas();
         }
         public void executaMovimento (Posicao origem, Posicao destino)
@@ -23,6 +25,50 @@ namespace Xadrez
             Peca pecaCapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
         }
+
+        public void realizaJogada (Posicao origem, Posicao destino)
+        {
+            executaMovimento(origem, destino);
+            turno++;
+            mudaJogador();
+        }
+
+        public void validarPosicaoDeOrigem(Posicao pos)
+        {
+            if (tab.peca(pos) == null)
+            {
+                throw new TabuleiroException("Não há peça na casa indicada!");
+            }
+            if (jogadorAtual != tab.peca(pos).cor)
+            {
+                throw new TabuleiroException("Escolha uma peça da sua cor!");
+            }
+            if (!tab.peca(pos).existeMovimentosPossiveis())
+            {
+                throw new TabuleiroException("A peça indicada está travada, não há opções de movimentos");
+            }
+        }
+
+        public void validarPosicaoDeDestino(Posicao origem, Posicao destino)
+        {
+            if(!tab.peca(origem).podeMoverPara(destino))
+            {
+                throw new TabuleiroException("Destino inválido!");
+            }
+        }
+
+        private void mudaJogador()
+        {
+            if(jogadorAtual == Cor.Branca)
+            {
+                jogadorAtual = Cor.Preta;
+            }
+            else
+            {
+                jogadorAtual = Cor.Branca;
+            }
+        }
+
         private void colocarPecas()
         {
             tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('c', 1).toPosicao());
