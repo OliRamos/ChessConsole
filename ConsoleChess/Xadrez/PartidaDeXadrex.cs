97cf9665f84c6ffc906523d5af41ca8ace2b1232
OliRamos <oli.ramos@ymail.com>
2023-06-28 12:25:21 -0300

    Jogada especial promocao

--- ConsoleChess/Xadrez/PartidaDeXadrex.cs
+++ ConsoleChess/Xadrez/PartidaDeXadrex.cs
@@ -136,6 +136,22 @@ namespace Xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode colocar-se em Xeque, faça outra jogada!");
             }
+
+            Peca p = tab.peca(destino);
+
+            //jogada especial promocao
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0 || p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -155,7 +171,7 @@ namespace Xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
 
             //Jogada especial En Passant
             if (p is Peao && (destino.linha == origem.linha -2 || destino.linha == origem.linha +2))
